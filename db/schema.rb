8d65ece8121bed4ec2fe4be48c71dd1c1361d225
Lisa Lee <snickers495@gmail.com>
2017-12-02 22:12:17 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171202221040) do
+ActiveRecord::Schema.define(version: 20171202221126) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20171202221040) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
-    t.boolean  "post_status"
+    t.string   "post_status"
   end
 
 end
